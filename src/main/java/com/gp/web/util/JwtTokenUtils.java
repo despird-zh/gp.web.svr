9c9cb4fe59f22ce3367a5cb5437688838b73c426
desprid-zh <desprid-zh@yahoo.com>
2016-12-29 17:15:18 +0800

    update

--- src/main/java/com/gp/web/util/JwtTokenUtils.java
+++ src/main/java/com/gp/web/util/JwtTokenUtils.java
@@ -1,6 +1,5 @@
 package com.gp.web.util;
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.Map;
 
 import org.apache.commons.collections.MapUtils;
@@ -9,7 +8,8 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;  
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.gp.common.JWTPayload;  
   
 
 public class JwtTokenUtils {
@@ -77,72 +77,5 @@ public class JwtTokenUtils {
     	}
     	
     } 
-    
-    /**
-     * This class wrap the setting of payload 
-     **/
-    public static class JWTPayload{
-    	
-    	private String issuer;
-    	private String audience;
-    	private Date expireTime;
-    	private Date notBefore;
-    	private String subject;
-    	private Date issueAt = new Date();
-    	private String jwtId;
-    	
-    	public String getSubject() {
-			return subject;
-		}
-		public void setSubject(String subject) {
-			this.subject = subject;
-		}
-		private Map<String, Object> claims;
-    	
-		public Map<String, Object> getClaims() {
-			return claims;
-		}
-		public void setClaims(Map<String, Object> claims) {
-			this.claims = claims;
-		}
-		
-		public String getIssuer() {
-			return issuer;
-		}
-		public void setIssuer(String issuer) {
-			this.issuer = issuer;
-		}
-		public String getAudience() {
-			return audience;
-		}
-		public void setAudience(String audience) {
-			this.audience = audience;
-		}
 
-		public Date getExpireTime() {
-			return expireTime;
-		}
-		public void setExpireTime(Date expireTime) {
-			this.expireTime = expireTime;
-		}
-		public Date getNotBefore() {
-			return notBefore;
-		}
-		public void setNotBefore(Date notBefore) {
-			this.notBefore = notBefore;
-		}
-		public Date getIssueAt() {
-			return issueAt;
-		}
-		public void setIssueAt(Date issueAt) {
-			this.issueAt = issueAt;
-		}
-		public String getJwtId() {
-			return jwtId;
-		}
-		public void setJwtId(String jwtId) {
-			this.jwtId = jwtId;
-		}
-
-    }
 }
