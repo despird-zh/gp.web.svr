dd2bc27ebba7bab07fe10f44f604573a777aa40b
desprid-zh <desprid-zh@yahoo.com>
2017-02-23 10:15:34 +0800

    update

--- src/main/java/com/gp/web/ActionResult.java
+++ src/main/java/com/gp/web/ActionResult.java
@@ -8,49 +8,83 @@ import java.util.Map;
  * 
  * @author diaogc
  * @version 0.1 2014-12-10
+ * 
  **/
 public class ActionResult{
 	
+	/** the meta information of result */
 	private Meta meta = null;
 
+	/** the data holder of result */
 	private Object data = null;
 
+	/**
+	 * Build a new failure state result with message
+	 * 
+	 * @param message the message string
+	 * @return ActionResult the action result
+	 **/
     public static ActionResult failure(String message) {
     	ActionResult ref = new ActionResult();
         ref.meta = new Meta(BaseController.FAIL, message);
         return ref;
     }
     
+	/**
+	 * Build a new error state result with message
+	 * 
+	 * @param message the message string
+	 * @return ActionResult the action result
+	 **/
     public static ActionResult error(String message) {
     	ActionResult ref = new ActionResult();
         ref.meta = new Meta(BaseController.ERROR, message);
         return ref;
     }
     
+	/**
+	 * Build a new success state result with message
+	 * 
+	 * @param message the message string
+	 * @return ActionResult the action result
+	 **/
     public static ActionResult success(String message) {
     	ActionResult ref = new ActionResult();
         ref.meta = new Meta(BaseController.SUCCESS, message);
         return ref;
     }
     
+    /**
+     * Get meta information
+     * @return Meta the meta  
+     **/
 	public Meta getMeta() {
 		return meta;
 	}
 
+	/**
+	 * Set meta information 
+	 **/
 	public void setMeta(Meta meta) {
 		this.meta = meta;
 	}
 
+	/**
+	 * Get the data information 
+	 **/
 	public Object getData() {
 		return data;
 	}
 
+	/**
+	 * Set the data information 
+	 **/
 	public void setData(Object data) {
 		this.data = data;
 	}	
 	
 	/**
-	 * convert the action result into map 
+	 * Convert the action result into a map, it to be easy to append to ModelAndView
 	 **/
 	public Map<String, Object> asMap(){
 		
@@ -66,31 +100,52 @@ public class ActionResult{
 	 **/
     public static class Meta {
 
+    	/** the state of meta */
         private String state;
+        /** the code of meta */
         private String code;
+        /** the message of meta */
         private String message;
 
+        /**
+         * Create Meta with state 
+         **/
         public Meta(String state) {
             this.state = state;
         }
 
+        /**
+         * Create Meata with state and message 
+         **/
         public Meta(String state, String message) {
             this.state = state;
             this.message = message;
         }
 
+        /**
+         * Get the state 
+         **/
         public String getState() {
             return state;
         }
 
+        /**
+         * Get the message 
+         **/
         public String getMessage() {
             return message;
         }
 
+        /**
+         * Get the code 
+         **/
 		public String getCode() {
 			return code;
 		}
 
+		/**
+		 * Set the code 
+		 **/
 		public void setCode(String code) {
 			this.code = code;
 		}
