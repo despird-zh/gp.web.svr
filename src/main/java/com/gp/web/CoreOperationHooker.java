7a16d2b04dcd4d3484ed255610252b981ff13f1a
gdiao <gary.diao@yahoo.com>
2017-09-06 14:10:13 +0800

    update

--- src/main/java/com/gp/web/CoreOperationHooker.java
+++ src/main/java/com/gp/web/CoreOperationHooker.java
@@ -9,29 +9,36 @@ import com.gp.disruptor.EventType;
 import com.gp.exception.CoreException;
 import com.gp.exception.RingEventException;
 
-public class CoreWebHooker extends EventHooker<EventPayload>{
+/**
+ * Here generate the operation log for resource, member, workgroup
+ * 
+ *  @author gdiao 
+ *  @version 0.1 2017-08-07
+ *  
+ **/
+public class CoreOperationHooker extends EventHooker<EventPayload> {
 
-	public CoreWebHooker(EventType eventType) {
+	public CoreOperationHooker(EventType eventType) {
 		super(eventType);
 	}
 
 	@Override
-	public void processPayload(EventPayload payload) throws RingEventException{
+	public void processPayload(EventPayload payload) throws RingEventException {
 
 		CoreEventLoad coreload = (CoreEventLoad) payload;
 		Operations operation = Operations.valueOf(coreload.getOperation());
 		try {
 			switch (operation) {
-				case UPDATE_BASIC_SETTING:
-					OperationFacade.handleUpdateAccount(coreload);
-					break;
-				case UPDATE_SYSOPTION:
-					OperationFacade.handleUpdateSysOption(coreload);
-					break;
-				default:
-					break;
+			case UPDATE_BASIC_SETTING:
+				OperationFacade.handleUpdateAccount(coreload);
+				break;
+			case UPDATE_SYSOPTION:
+				OperationFacade.handleUpdateSysOption(coreload);
+				break;
+			default:
+				break;
 			}
-		}catch(CoreException ce){
+		} catch (CoreException ce) {
 
 			throw new RingEventException("Fail handle core event payload", ce);
 		}
